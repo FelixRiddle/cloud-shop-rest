cea3afe36088e686568ce0dc62fd392f2661d61f
Felix <fedecraft3312@gmail.com>
2024-07-16 09:53:38 -0300

    Get product by id

--- src/server/routes/product.js
+++ src/server/routes/product.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const { createProductFolder, productFolder } = require("../../lib/folder/product");
 const productImage = require("../../lib/upload/productImage");
+const expandData = require("../../lib/misc/expandData");
 
 /**
  * Products router
@@ -11,6 +12,7 @@ const productImage = require("../../lib/upload/productImage");
 function productRouter() {
 	const router = express.Router();
 	
+	// Create product
 	router.post("/", productImage, async (req, res) => {
 		try {
 			const {
@@ -50,6 +52,72 @@ function productRouter() {
 					type: "success"
 				}]
 			});
+		} catch(err) {
+			console.error(err);
+			return res
+				.status(500)
+				.send({
+					messages: [{
+						message: "Error 500: Internal error",
+						type: "error"
+					}]
+				});
+		}
+	});
+	
+	// Show all products
+	// TODO: Pagination
+	router.get("/", async(req, res) => {
+		try {
+			const {
+				Product
+			} = req.models;
+			
+			const products = await Product.find({});
+			
+			return res.send({
+				products
+			});
+		} catch(err) {
+			console.error(err);
+			return res
+				.status(500)
+				.send({
+					messages: [{
+						message: "Error 500: Internal error",
+						type: "error"
+					}]
+				});
+		}
+	});
+	
+	// Show a product by id
+	router.get("/:productId", async (req, res) => {
+		try {
+			const {
+				Product
+			} = req.models;
+			const productId = req.params.productId;
+			const product = await Product.findById(productId);
+			
+			if(!product) {
+				req.flash("messages", [{
+					message: "Product doesn't exists",
+					type: "error"
+				}]);
+				
+				const extra = await expandData(req);
+				return res
+					.status(404)
+					.send({
+						...extra,
+					});
+			}
+			
+			return res
+				.send({
+					product,
+				});
 		} catch(err) {
 			console.error(err);
 			return res
