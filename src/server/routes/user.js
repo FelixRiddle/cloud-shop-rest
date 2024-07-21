9df1df9b09a377198f4b5f93a11f7ee19a60e8cf
Felix <fedecraft3312@gmail.com>
2024-07-21 20:21:43 -0300

    Logout

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -15,6 +15,34 @@ function userRouter() {
 			});
 	});
 	
+	router.get("/logout", async (req, res) => {
+		try {
+			req.session.destroy();
+			
+			// Flash doesn't work without the sessions
+			return res.send({
+				messages: [{
+					message: "Logged out",
+					type: "success"
+				}]
+			});
+		} catch(err) {
+			console.error(err);
+			
+			req.flash("messages", [{
+				message: "Error 500: Internal error",
+				type: "error"
+			}]);
+			
+			const extra = await expandData(req);
+			return res
+				.status(500)
+				.send({
+					...extra
+				});
+		}
+	});
+	
 	return router;
 }
 
